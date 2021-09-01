b379b688bbbb095c8f323ed03da2545244a3f3a6
Daniele Tabanella <daniele.tabanella@gmail.com>
2021-09-01 22:33:15 +0200

    feat(comps): add more comps

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Button from '@/components/Button/Button.vue';
+import Code from '@/components/Code/Code.vue';
+import Input from '@/components/Input/Input.vue';
 
 const Components = {
   Button,
+  Code,
+  Input,
 };
 
 Object.keys(Components).forEach((name) => {
